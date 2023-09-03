2c5561e99633ad5b7de8ca54caea513642a76d96
서지호 <103229466+jihoseo2006@users.noreply.github.com>
2023-09-03 15:52:09 +0900

    컬럼 작성 페이지 요소 생성

---
+++ project/column/writecolumn/writeColumn.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $('#write').summernote({
+    focus: true,
+    lang: 'ko-KR',
+    placeholder: '내용을 작성하세요',
+    width: '70%',
+    height: '350px',
+  });
+});
+
+
