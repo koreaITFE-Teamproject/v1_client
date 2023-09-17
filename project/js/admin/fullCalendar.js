e219ee4fde1f8abc66d9aca2d3af4d357a745e3f
서지호 <103229466+jihoseo2006@users.noreply.github.com>
2023-09-18 08:05:44 +0900

    9.17 fullcalendar 작업

--- project/js/admin/fullCalendar.js
+++ project/js/admin/fullCalendar.js
@@ -3,10 +3,14 @@
 document.addEventListener('DOMContentLoaded', function() {
   var calendarEl = document.getElementById('calendar');
   var calendar = new FullCalendar.Calendar(calendarEl, {
-    initialView: 'dayGridMonth'
+    initialView: 'dayGridMonth', // 달 뷰 형식
+    locale: 'ko', // 한국어 설정
+    aspectRatio: 0.6, //달력 비율
+    
+    
   });
   calendar.render();
 });
 
-// 라이브러리 객체 소스 가지고 요소의 절대적 크기를 줄인다음에 css를 가지고 놀아야함. css만으로는 자동으로 사이즈 확대/축소가 안됨.
+
 
