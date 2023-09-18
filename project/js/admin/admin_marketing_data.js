06bf839bcbbee11e547b4820d170a9370bfd3651
jangtjdgns <jangtjdgns0208@gmail.com>
2023-09-18 13:55:44 +0900

    관리자_마케팅 데이터 화면 차트 추가
    23.09.18(월)
    -bootstrap 대시보드 차트만 추가
    -그 외 CSS폰트적용

---
+++ project/js/admin/admin_marketing_data.js
@@ -0,0 +1,46 @@
+$(function () {
+    'use strict';
+
+    // 그래프
+    var ctx = $('#myChart')[0].getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: [
+                'Sunday',
+                'Monday',
+                'Tuesday',
+                'Wednesday',
+                'Thursday',
+                'Friday',
+                'Saturday'
+            ],
+            datasets: [{
+                data: [
+                    15339,
+                    21345,
+                    18483,
+                    24003,
+                    23489,
+                    24092,
+                    12034
+                ],
+                tension: 0,
+                backgroundColor: 'transparent',
+                borderColor: '#007bff',
+                borderWidth: 4,
+                pointBackgroundColor: '#007bff'
+            }]
+        },
+        options: {
+            plugins: {
+                legend: {
+                    display: false
+                },
+                tooltip: {
+                    boxPadding: 3
+                }
+            }
+        }
+    });
+});
