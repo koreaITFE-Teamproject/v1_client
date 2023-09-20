a3782858b1e91de66f1eac3856e85a108dba4fff
jangtjdgns <jangtjdgns0208@gmail.com>
2023-09-20 15:58:30 +0900

    CSS 수정
    23.09.20(수)
    -가입, 수정, 로그인, 탈퇴, 토론방 CSS 수정
    -그 외 탈퇴페이지 js 문법을 jquery 문법으로 변경

--- project/js/user/deleteAccount.js
+++ project/js/user/deleteAccount.js
@@ -1,54 +1,32 @@
+// jquery로 변경
 $(function () {
-    const searchButton = document.querySelector('#withdrawButton');
-
-    searchButton.addEventListener('click', (e) => {
+    $("#withdrawButton").click(function (e){
         e.preventDefault();
-        AlertCheckbox()	// all unchecked 일 때, 이 함수 소환!
-    })
+        AlertCheckbox();
+    });
 
-    //😍 ALL NO checked 함수
+    // ALL NO checked 함수
     function AlertCheckbox() {
-        const checkPart = document.querySelector('.checkbox-part');
-        const checkboxes = checkPart.querySelectorAll('input');
+        const checkboxes = $(".checkbox-part input");
         let checkedCnt = 0;
 
-        //😍 이렇게 바꿨어요!
         for (let i = 0; i < checkboxes.length; i++) {
             if (checkboxes[i].checked === true) {
-                checkedCnt++
+                checkedCnt++;
             }
         }
 
+        // 아무것도 선택 안했을 시
         if (checkedCnt == 0) {
-            alert("체크박스를 선택해주세요.")
-            return;
+            return alert("체크박스를 선택해주세요.")
         }
 
-        // 체크없으면 바로 return해서 alert 띄우기!
-
         // 비밀번호 입력 확인
-        var passwordInput = document.getElementById('passwordInput').value;
-        if (passwordInput.trim() === '') {
-            // 비밀번호가 비어있을 때 경고 메시지 표시
-            alert('비밀번호를 작성해 주세요.');
-            return;
+        if (!$("#passwordInput").val().trim()) {         // 실패 시
+            $("#passwordInput").val("");                 // 초기화
+            return alert('비밀번호를 작성해 주세요.');     // 비밀번호가 비어있을 때 경고 메시지 표시
         }
 
         alert('탈퇴되었습니다.');
     }
-
-
-
-
-
-
-    // var checkbox = document.getElementById('reasonCheckbox');
-    // if (!checkbox.checked) {
-    //     // 체크되지 않았을 때 경고 메시지 표시
-    //     alert('체크박스를 선택하세요.');
-    //     return;
-    // }
-
-
-    // });
 });
