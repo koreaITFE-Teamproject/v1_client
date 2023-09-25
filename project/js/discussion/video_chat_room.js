cac43e2c48c0821abd6bcbe156d74540de5ccde8
jangtjdgns <jangtjdgns0208@gmail.com>
2023-09-25 13:49:13 +0900

    CSS 수정
    23.09.25(월)
    - 수정, 로그인, 회원가입, 화상토론방, 회원 조회 CSS 수정
    - 새 토론방은 후에 수정 예정
    - 그 외 로그인 이미지 추가, 화상토론방 마이크, 캠 js 작성

--- project/js/discussion/video_chat_room.js
+++ project/js/discussion/video_chat_room.js
@@ -1,8 +1,23 @@
-$(function(){
+$(function () {
     $(".options>div>i:nth-child(2)").hide();
 
 
-    $(".options>div").click(function(){
-       $(this).children("i").toggle();
+    // $(".options>div").click(function () {
+    //     $(this).children("i").toggle();
+    // });
+
+
+    // 일단 중복되게 구현함, 시간나면 수정 예정
+    $("#notification-toggle-switch-1").click(function () {
+        toggleSWT($(this));
     });
+
+    $("#notification-toggle-switch-2").click(function () {
+        toggleSWT($(this));
+    });
+
+    function toggleSWT($this) {
+        $this.toggleClass("toggle-swt-checked");
+        $this.children().toggleClass("toggle-btn-checked");
+    }
 });
