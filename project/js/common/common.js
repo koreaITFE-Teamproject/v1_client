4f3b375afd1e5ba6aeed1d3045f67d63c956b717
jangtjdgns <jangtjdgns0208@gmail.com>
2023-10-03 15:53:24 +0900

    헤더 스크롤 수정
    23.10.03(화)
    -헤더 스크롤 수정
    -마진 탑 수정해야할듯

--- project/js/common/common.js
+++ project/js/common/common.js
@@ -1,4 +1,9 @@
 $(function () {
   $("#header").load("/project/views/common/header.html");
   $("#footer").load("/project/views/common/footer.html");
+
+  // header position fixed로 인한 겹침을 헤더의 높이만큼 전체 마진줌 + 30px
+  // 130으로 줬을때 마진탑이 60px으로 적용되는 페이지도 있음- 이부분만 수정해도될듯함
+  $("main").css("margin-top", "130px");
+  
 });
