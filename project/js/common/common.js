20cd198d21a2a6a9f500bb034404e71ec52a5d7d
joje3029 <joje3029@gmail.com>
2023-10-04 18:09:41 +0900

    2023.10.04 박소민 작업 푸시
    버튼 수정사항 적용. h1 적용. 메인 문구 적용. 캐러셀은 성훈님이 해보고 봐주기로함.
    칼럼 읽기 댓글수 제한 기능 만드는중. 태진쌤 조언에 따라 매번 세는 쪽으로 만드는 중

--- project/js/common/common.js
+++ project/js/common/common.js
@@ -6,4 +6,16 @@ $(function () {
   // 130으로 줬을때 마진탑이 60px으로 적용되는 페이지도 있음- 이부분만 수정해도될듯함
   $("main").css("margin-top", "130px");
   
+  $(".textarea").on("keypress",function(e){
+    var content = $(this).val();
+		$(".js_num").text(content.length); //실시간 글자수 카운팅
+		if ($(this).val().length > 300) {
+      e.preventDefault();
+			alert("최대 300자까지 입력 가능합니다.");
+			// 현상태 문제 글자 수가 지워졌을때 글자수 변경 안됨.
+      // 태진쌤 조언 : 드래그로 없앨수 있으니 그때그때마다 애가 숫자를 세도록 해야한다.
+      // (this).val().length를 매번 세는 로직을 만들어야함.
+		}
+  })
+
 });
