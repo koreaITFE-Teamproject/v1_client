0eaf3ef9b7fce45718d3178758a443536ec87741
joje3029 <127838357+joje3029@users.noreply.github.com>
2023-10-07 15:44:16 +0900

    2023.10.07 박소민 플로팅 배너 링크 연결
    플로팅 배너 챗봇 고객센터로 연결되게 링크 함.

--- project/js/customer_center_page/chatbot_page.js
+++ project/js/customer_center_page/chatbot_page.js
@@ -6,10 +6,4 @@ $(document).ready(function () {
     $('.chat_q').mouseover(function () {
         $(this).css('cursor', 'pointer');
     })
-
-    // 1:1 고객상담페이지 클릭시 작동되는 코드
-    $('#one_by_one').on('click', function () {
-        alert('고객상담센터')
-        // 고객 상담페이지로 연결되게 수정해야함.
-    })
 })
